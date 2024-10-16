93d66c82917bd831251a74a1bcbb2cb6f183bc0e
minh1510 <nguyennhuminh2002.com>
2024-10-16 12:36:12 +0700

    sualoideploy2

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("db_ban_dong_ho", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
+// const sequelize = new Sequelize("db_ban_dong_ho", "root", null, {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE || "railway", 
+  process.env.MYSQL_USER || "root", 
+  process.env.MYSQL_ROOT_PASSWORD || "PXzNkzlUrFIoRSpTkNOeIZeVXXlBixjR", 
+  {
+host: "mysql.railway.internal",  // Địa chỉ host của Railway
+dialect: "mysql",
+port: process.env.MYSQL_PORT || 3306,
 });
 const connection = async () => {
   try {
